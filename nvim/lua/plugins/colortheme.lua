f3349f32e0242211c576ae79f4d88f0e4afc807a
samuel <owen84571@gmail.com>
2025-02-09 03:21:34 +0530

    few more configs

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,34 +1,33 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000, -- Ensure it loads first
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha",
-                background = { light = "latte", dark = "mocha" },
-                transparent_background = false,
-                show_end_of_buffer = false,
-                term_colors = false,
-                dim_inactive = { enabled = false, shade = "dark", percentage = 0.15 },
-                no_italic = false,
-                no_bold = false,
-                no_underline = false,
-                styles = {
-                    comments = { "italic" },
-                    conditionals = { "italic" },
-                },
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    treesitter = true,
-                    notify = false,
-                    mini = { enabled = true, indentscope_color = "" },
-                },
-            })
-            vim.cmd.colorscheme("catppuccin")
-        end,
-    },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha',
+        background = { light = 'latte', dark = 'mocha' },
+        transparent_background = true,
+        show_end_of_buffer = false,
+        term_colors = false,
+        dim_inactive = { enabled = false, shade = 'dark', percentage = 0.15 },
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+        styles = {
+          comments = { 'italic' },
+          conditionals = { 'italic' },
+        },
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = { enabled = true, indentscope_color = '' },
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
 }
-
