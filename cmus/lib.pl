ff0a48e6ee23724484fd870ddd96588aef6e5f92
samuel <owen84571@gmail.com>
2025-08-04 15:45:11 +0530

    usual

--- cmus/lib.pl
+++ cmus/lib.pl
@@ -1,9 +1,9 @@
-/home/samuel/Videos/music/Dil sambhal jaa zara.mp3
-/home/samuel/Videos/music/Dil Sambhal Ja Zara ｜ LYRICS ｜ Arijit Singh, Mohammad Irfan Ali, Saim Bhat [r_3zVIyblLQ].mp3
-/home/samuel/Videos/music/Free fire.mp3
-/home/samuel/Videos/music/Pal Pal.mp3
-/home/samuel/Videos/music/Rakh lo tum chupake.mp3
-/home/samuel/Videos/music/samurai lofi.mp3
-/home/samuel/Videos/music/Spacewave.mp3
-/home/samuel/Videos/music/The Lost Soul Down X Lost Soul.mp3
-/home/samuel/Videos/music/Unki mehfil mein.mp3
+/home/samuel/music/Afusic - Pal Pal with @Talwiinder  (Official Visualiser) Prod. @AliSoomroMusic [AbkEmIgJMcU].mp3
+/home/samuel/music/Dil sambhal jaa zara.mp3
+/home/samuel/music/Free fire.mp3
+/home/samuel/music/Pal Pal.mp3
+/home/samuel/music/Rakh lo tum chupake.mp3
+/home/samuel/music/samurai lofi.mp3
+/home/samuel/music/Spacewave.mp3
+/home/samuel/music/The Lost Soul Down X Lost Soul.mp3
+/home/samuel/music/Unki mehfil mein.mp3
